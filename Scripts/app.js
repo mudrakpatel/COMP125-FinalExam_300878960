d06d24c7fd824d0b123545c200a38819255bc980
mudrakpatel <mudrakpatel96@gmail.com>
2016-08-17 13:51:47 -0500

    Second Commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,21 +1,47 @@
 var core;
-(function (core) {
+(function(core) {
     "use strict";
-    var canvas;
+    var canvas = document.getElementById("canvas");
     var stage;
+    var RollButton = new createjs.Bitmap("Assets/images/rollButton.png");
     // app entry function
     function init() {
         canvas = document.getElementById("canvas");
         stage = new createjs.Stage(canvas);
+        stage.addchild(RollButton);
         stage.enableMouseOver(20);
         createjs.Ticker.framerate = 60;
         createjs.Ticker.on("tick", gameLoop);
         main();
     }
+
     function gameLoop() {
+
         stage.update();
     }
+
     function main() {
+
+    }
+
+    function RollButtonClicked() {
+        var RandomNumber = Math.floor(Math.random() * 6) + 1;
+        switch (RandomNumber) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            default:
+                break;
+        }
     }
     window.addEventListener("load", init);
 })(core || (core = {}));
