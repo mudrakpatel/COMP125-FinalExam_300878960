065b8641df022805c2f1e84d35ef77ee04150eea
mudrakpatel <mudrakpatel96@gmail.com>
2016-08-17 14:17:49 -0500

    Final documentation

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,8 @@
+/**
+ * @ Author: Mudrak Patel
+ * @ student ID: 300878960
+ * @ Comp125_ClientSideWebDevelopment
+ */
 var core;
 (function(core) {
     "use strict";
@@ -7,6 +12,11 @@ var core;
     var RollButton = new createjs.Bitmap("Assets/images/rollButton.png");
     RollButton.x = 200;
     RollButton.y = 400;
+    var Dice1 = new createjs.Bitmap("Assets/images/blank.png");
+    Dice1.x = 100;
+    Dice1.y = 200;
+
+    var Dice2 = new createjs.Bitmap("Assets/images/blank.png");
     // app entry function
     function init() {
         canvas = document.getElementById("canvas");
@@ -27,7 +37,11 @@ var core;
     function main() {
 
     }
-
+    /**
+     * @ function : RollButtonClicked
+     * @ return : void
+     * @ purpose : handles the click event of the RollButton
+     */
     function RollButtonClicked() {
         var RandomNumber = Math.floor(Math.random() * 6) + 1;
         console.log("The random number is: " + RandomNumber);
